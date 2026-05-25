072687ddbf2a6b9592e4546671e8fd2bb310872f
Emmszhii <emmanuelmanalo707@gmail.com>
2026-05-25 19:43:10 +0800

    update ui

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Data from "./pages/data/Data";
